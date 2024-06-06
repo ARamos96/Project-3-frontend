a61aa4fa5f0ebcceac8c44bcdcf654406203b59f
Arnau Ramos <arnauramos21@gmail.com>
2024-06-06 12:26:46 +0200

    created recipespage,jsx and recipespage.css files

---
+++ src/pages/RecipesPage/RecipesPage.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import { Link } from "react-router-dom";
+
+const MONGO_URI =  process.env.MONGODB_URI 
+
+
+
+function RecipesPage() {
+  return (
+    <div>RecipesPage</div>
+  )
+}
+
+export default RecipesPage
