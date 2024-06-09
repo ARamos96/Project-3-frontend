ea5d84cff41c4a36413aea32f01563805b0469b0
Abraham Agusti <abraham.agusti@gmail.com>
2024-06-09 18:50:18 +0200

    FooterComponent

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './footer.css'
+
+function Footer() {
+  return (
+
+    <div>
+        <Link>How it works</Link>
+        <Link>Dishes</Link>
+        <Link>Subscriptions</Link>
+        <Link>Create account</Link>
+        <Link>X</Link>
+        <Link>Facebook</Link>
+        <Link>Instagram</Link>    
+
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
 import "./HomePage.css";
+import "./../../components/Footer.jsx"
+
 
 function HomePage() {
   return (
     <div>
       <h1>Home page</h1>
+
+      <Footer />
     </div>
   );
 }
