dc5ed6fa2be4874a7fefd32bf7b367f963222375
Arnau Ramos <arnauramos21@gmail.com>
2024-06-11 14:49:31 +0200

    Recipe-details-page mostly done. Pending CSS

--- src/pages/RecipeDetailsPage/RecipeDetailsPage.jsx
+++ src/pages/RecipeDetailsPage/RecipeDetailsPage.jsx
@@ -20,9 +20,79 @@ function RecipeDetailsPage() {
   }, [recipeId]);
 
   return (
-    <div>
+    <div className="recipe-details">
       <h1>{recipe.name}</h1>
-      <p>{recipe.description}</p>
+      <div className="1st-recipe-section">
+        <img src={recipe.bigImageURL} alt={recipe.name}></img>
+        <div className="recipe-basic-info">
+          <p>{recipe.cookingTime}</p>
+          <p>{recipe.rating}</p>
+          <p>{recipe.difficulty}</p>
+        </div>
+        {/* {isSubscribed ? 
+      <Link>Add To Subscription</Link>
+      : <Link>Let's get started!</Link>} */}
+      </div>
+      <div className="recipe-tags">
+        {recipe.categories && (
+          <>
+            {recipe.categories.origin.map((origin, index) => (
+              <p key={`origin-${index}`}>{origin}</p>
+            ))}
+            {recipe.categories.diet.map((diet, index) => (
+              <p key={`diet-${index}`}>{diet}</p>
+            ))}
+          </>
+        )}
+      </div>
+      <div className="recipe-ingredients-container">
+        <h2>Ingredients</h2>
+        <ul className="ingredients-list">
+          {recipe.ingredients &&
+            recipe.ingredients.map((ingredient, index) => (
+              <li key={`ingredient-${index}`}>{ingredient}</li>
+            ))}
+        </ul>
+      </div>
+      {/* Obtain Nutritional Information from Object */}
+      {recipe.nutritionalValuePerServing && (
+        <div className="nutritional-information-table">
+          <h2>Nutritional Information</h2>
+          <table>
+            <tbody>
+              <tr>
+                <th>Calories</th>
+                <td>{recipe.nutritionalValuePerServing.calories}kcal</td>
+              </tr>
+              <tr>
+                <th>Protein</th>
+                <td>{recipe.nutritionalValuePerServing.protein}g</td>
+              </tr>
+              <tr>
+                <th>Carbohydrates</th>
+                <td>{recipe.nutritionalValuePerServing.carbohydrates}g</td>
+              </tr>
+              <tr>
+                <th>Fat</th>
+                <td>{recipe.nutritionalValuePerServing.fat}g</td>
+              </tr>
+              <tr>
+                <th>Fiber</th>
+                <td>{recipe.nutritionalValuePerServing.fiber}g</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+      {recipe.steps && (
+  <ul className="recipe-steps">
+    {recipe.steps.map((step, index) => (
+      <li key={`step-${index}`}>
+        <span><b>Step {index + 1}</b> {step}</span>
+      </li>
+    ))}
+  </ul>
+)}
     </div>
   );
 }
