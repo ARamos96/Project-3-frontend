e36c85462f4e53c3790aed2bca4f39d17f711910
Arnau Ramos <arnauramos21@gmail.com>
2024-06-11 14:06:09 +0200

    created class for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import './Footer.css'
 function Footer() {
   return (
 
-    <div>
+    <div className='Footer'>
         <Link>How it works</Link>
         <Link>Dishes</Link>
         <Link>Subscriptions</Link>
