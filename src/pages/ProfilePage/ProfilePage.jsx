4f919b297a7dcd4e92c8ea20c57724f5242df4ad
guzegui <guzmanegui@gmail.com>
2024-06-12 20:19:46 +0200

    Initialize profile page jsx and css

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,41 @@
 import "./ProfilePage.css";
+import { useState, useContext } from "react";
+import authService from "../../services/auth.service";
+import { AuthContext } from "../../context/auth.context";
+import "./ProfilePage.css";
 
 function ProfilePage() {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Profile page</h1>
+    <div className="profile-page">
+      <h1>Profile Page</h1>
+      <div className="profile-info">
+        <div className="profile-item">
+          <strong>Name:</strong> {user.name}
+          <button>Edit</button>
+        </div>
+        <div className="profile-item">
+          <strong>Last Name:</strong> {user.lastName}
+          <button>Edit</button>
+        </div>
+        <div className="profile-item">
+          <strong>Email:</strong> {user.email}
+        </div>
+        <div className="profile-item">
+          <strong>Address:</strong> <button>View</button>
+        </div>
+        <div className="profile-item">
+          <strong>Payment Method:</strong> <button>View</button>
+        </div>
+        <div className="profile-item">
+          <strong>Active Subscription:</strong> <button>View</button>
+        </div>
+      </div>
     </div>
   );
 }
