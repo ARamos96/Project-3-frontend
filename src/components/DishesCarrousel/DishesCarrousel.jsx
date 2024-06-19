5cd32f6147f66f4896c9a5038fc58ab59d0e0077
Abraham Agusti <abraham.agusti@gmail.com>
2024-06-19 20:48:28 +0200

    Organizing Css

--- src/components/DishesCarrousel/DishesCarrousel.jsx
+++ src/components/DishesCarrousel/DishesCarrousel.jsx
@@ -6,7 +6,20 @@ function DishesCarrousel() {
   return (
     <div>
       <h3>Look some of our dishes</h3>
-      
+      <section>
+        <div>
+          <img src="" alt="Image 1" />
+          <p>Dish 1</p>
+        </div>
+        <div>
+          <img src="" alt="Image 2" />
+          <p>Dish 2</p>
+        </div>
+        <div>
+          <img src="" alt="Image 2" />
+          <p>Dish 2</p>
+        </div>
+      </section>
 
     </div>
   )
