ffed6015f0c8dc41c15e550b0f73f7eebef5a24e
Abraham Agusti <abraham.agusti@gmail.com>
2024-06-19 22:19:06 +0200

    Loader and contitional for the recipies buttons

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,34 +1,38 @@
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/auth.context";
 import { CartContext } from "../../context/cart.context";
 import "./RecipeList.css";
-import "primeicons/primeicons.css"
+import "primeicons/primeicons.css";
 
-const MONGO_URI = `${process.env.REACT_APP_SERVER_URL}/dishes` || "http://localhost:5005/dishes";
+import Loading from "../Loading/Loading";
+
+const MONGO_URI = process.env.REACT_APP_SERVER_URL
+  ? `${process.env.REACT_APP_SERVER_URL}/dishes`
+  : "http://localhost:5005/dishes";
 
 function RecipesList() {
-   // Subscribe to the AuthContext to gain access to
-  // the values from AuthContext.Provider's `value` prop
   const { isLoggedIn } = useContext(AuthContext);
   const { addToCart, mealPlan } = useContext(CartContext);
   const navigate = useNavigate();
 
-  // Define recipes
+  const [loading, setLoading] = useState(true);
   const [recipes, setRecipes] = useState([]);
 
-  // Get recipes from the database
   useEffect(() => {
     axios
       .get(MONGO_URI)
       .then((res) => {
         setRecipes(res.data);
+        setLoading(false);
       })
-      .catch((err) => console.error('Error fetching recipes:', err));
+      .catch((err) => {
+        console.error("Error getting the recipes:", err);
+        setLoading(false);
+      });
   }, []);
 
-  // Handler function to add recipe to the cart
   const handleAddToCart = (recipe) => {
     if (!mealPlan || !mealPlan.dishesPerWeek) {
       navigate("/mealplan");
@@ -39,23 +43,35 @@ function RecipesList() {
 
   return (
     <div className="recipe-menu">
-      {recipes &&
+      {loading ? (
+        <Loading />
+      ) : (
         recipes.map((recipe) => (
-          <div className="recipe-container"  key={recipe._id}>
+          <div className="recipe-container" key={recipe._id}>
             <Link to={`/recipes/${recipe._id}`}>
-              <img src={recipe.smallImageURL} alt={`${recipe.name}`}></img>
+              <img src={recipe.smallImageURL} alt={`${recipe.name}`} />
               <p>{recipe.name}</p>
               <div className="recipe-info">
-                <p><span className="pi pi-stopwatch" /> {recipe.cookingTime}'</p>
+                <p>
+                  <span className="pi pi-stopwatch" /> {recipe.cookingTime}'
+                </p>
                 <p>{recipe.nutritionalValuePerServing.calories}kcal</p>
-                <p>{recipe.rating} <span className="pi pi-star-fill" /></p>
+                <p>
+                  {recipe.rating} <span className="pi pi-star-fill" />
+                </p>
               </div>
             </Link>
-            {isLoggedIn ? 
-            <button className='subscription-button' onClick={() => handleAddToCart(recipe)}>Add to Subscription</button> 
-            : ""}
+            {isLoggedIn && mealPlan && mealPlan.dishesPerWeek ? (
+              <button
+                className="subscription-button"
+                onClick={() => handleAddToCart(recipe)}
+              >
+                Add to Subscription
+              </button>
+            ) : null}
           </div>
-        ))}
+        ))
+      )}
     </div>
   );
 }
