1e9425dc506eb7efb3e4d0b03ca865680396ce05
Arnau Ramos <arnauramos21@gmail.com>
2024-06-20 13:08:09 +0200

    Changed text on homepage and HIW page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,6 @@ import "./HomePage.css";
 import { Link } from "react-router-dom";
 import DishesCarrousel from "../../components/DishesCarrousel/DishesCarrousel";
 
-
 function HomePage() {
   return (
     <div>
@@ -14,12 +13,10 @@ function HomePage() {
       <section>
         <h2>What is Savor Swift?</h2>
         <p>
-          Savor Swift is an online meal delivery service dedicated to bringing a diverse array of freshly prepared, gourmet meals right to your doorstep. Our mission is to provide a convenient and high-quality dining experience by using locally-sourced, organic ingredients and catering to a wide range of dietary preferences and needs.
-          At Savor Swift, we offer a variety of diets to suit every lifestyle, including vegan, vegetarian, gluten-free, keto, low-calories, paleo, and dairy-free options. We understand that maintaining a specific diet can be challenging, so we make it easy to enjoy delicious, nutritious meals that align with your dietary goals.
-          Our menu spans a multitude of global cuisines, ensuring there's something for everyone. Indulge in the vibrant and healthful dishes of the Mediterranean, savor the rich and comforting flavors of Italian cuisine, experience the sophisticated and elegant tastes of French cooking, or explore the diverse and aromatic dishes from Asia, Mexico, and India. Whether you're in the mood for a Greek salad, spaghetti carbonara, coq au vin, teriyaki salmon, tacos, or butter chicken, Savor Swift has you covered.
-          Savor Swift is designed to be user-friendly and convenient. Simply browse our extensive menu, place your order with your preferred customizations, and select your delivery time and location. Our expert chefs will meticulously prepare your meals, and our reliable delivery team will ensure they arrive at your door fresh and ready to enjoy.
-          Join our community to receive exclusive offers, updates on new menu items, and cooking tips from our chefs. Follow us on social media to stay connected and share your Savor Swift experience with friends and family.
-          Savor Swift – bringing the joy of delicious, home-cooked meals to your doorstep. Order today and taste the difference!
+          SavourSwift is a meal subscription service that provides
+          nutritionist-designed recipes, allowing you to mix and match meals to
+          fit your lifestyle. We deliver quality ingredients right to your
+          doorstep, making healthy eating easy and convenient.
         </p>
       </section>
 
@@ -28,15 +25,30 @@ function HomePage() {
         <div className="HowItWorksstepper">
           <div>
             <h5>Choose</h5>
-            <p><span>"Pick a diet preference, or mix and match tasty nutritionist-designed recipes to fit your lifestyle."</span></p>
+            <p>
+              <span>
+                "Pick a diet preference, or mix and match tasty
+                nutritionist-designed recipes to fit your lifestyle."
+              </span>
+            </p>
           </div>
           <div>
             <h5>Cook</h5>
-            <p><span>"Receive quality ingredients and easy-to-follow recipes, with most dinners ready in 30 minutes or less."</span></p>
+            <p>
+              <span>
+                "Receive quality ingredients and easy-to-follow recipes, with
+                most dinners ready in 30 minutes or less."
+              </span>
+            </p>
           </div>
           <div>
             <h5>Enjoy</h5>
-            <p><span>"You get the right portion size, carbs, protein, fats, and calories to align with your health goals."</span></p>
+            <p>
+              <span>
+                "You get the right portion size, carbs, protein, fats, and
+                calories to align with your health goals."
+              </span>
+            </p>
           </div>
         </div>
 
@@ -44,7 +56,6 @@ function HomePage() {
           <button> See more! </button>
         </Link>
       </section>
-
 
       <section>
         <h2>Dishes</h2>
@@ -57,11 +68,6 @@ function HomePage() {
       </section>
 
       <br />
-
-
-
-
-
     </div>
   );
 }
--- src/pages/HowItWorksPage/HowItWorksPage.jsx
+++ src/pages/HowItWorksPage/HowItWorksPage.jsx
@@ -41,15 +41,17 @@ function HowItWorksPage() {
 
         <AccordionComponent
           question="What is SavorSwift?"
-          answer="SavourSwift is a meal subscription service that provides
-  nutritionist-designed recipes, allowing you to mix and match meals
-  to fit your lifestyle. We deliver quality ingredients right to your
-  doorstep, making healthy eating easy and convenient."
+          answer="Savor Swift is an online meal delivery service dedicated to bringing a diverse array of freshly prepared, gourmet meals right to your doorstep. Our mission is to provide a convenient and high-quality dining experience by using locally-sourced, organic ingredients and catering to a wide range of dietary preferences and needs.
+          At Savor Swift, we offer a variety of diets to suit every lifestyle, including vegan, vegetarian, gluten-free, keto, low-calories, paleo, and dairy-free options. We understand that maintaining a specific diet can be challenging, so we make it easy to enjoy delicious, nutritious meals that align with your dietary goals.
+          Our menu spans a multitude of global cuisines, ensuring there's something for everyone. Indulge in the vibrant and healthful dishes of the Mediterranean, savor the rich and comforting flavors of Italian cuisine, experience the sophisticated and elegant tastes of French cooking, or explore the diverse and aromatic dishes from Asia, Mexico, and India. Whether you're in the mood for a Greek salad, spaghetti carbonara, coq au vin, teriyaki salmon, tacos, or butter chicken, Savor Swift has you covered.
+          Savor Swift is designed to be user-friendly and convenient. Simply browse our extensive menu, place your order with your preferred customizations, and select your delivery time and location. Our expert chefs will meticulously prepare your meals, and our reliable delivery team will ensure they arrive at your door fresh and ready to enjoy.
+          Join our community to receive exclusive offers, updates on new menu items, and cooking tips from our chefs. Follow us on social media to stay connected and share your Savor Swift experience with friends and family.
+          Savor Swift – bringing the joy of delicious, home-cooked meals to your doorstep. Order today and taste the difference!"
         />
 
         <AccordionComponent
           question="How does the subscription work?"
-          answer="When you subscribe to SavourSwift, you can choose a meal plan that
+          answer="When you subscribe to SavorSwift, you can choose a meal plan that
   fits your dietary preferences and schedule. Every week, you'll
   receive a box with fresh ingredients and step-by-step recipes to
   create delicious and nutritious meals at home."
@@ -59,20 +61,20 @@ function HowItWorksPage() {
           question="Can I customize my meal plan?"
           answer="Yes, you can customize your meal plan based on your dietary needs
   and preferences. Whether you are vegetarian, vegan, gluten-free, or
-  have other specific requirements, SavourSwift offers a variety of
+  have other specific requirements, SavorSwift offers a variety of
   options to ensure your meals are tailored to your lifestyle."
         />
 
         <AccordionComponent
           question="What if I have food allergies?"
-          answer="SavourSwift takes food allergies seriously. During the sign-up
+          answer="SavorSwift takes food allergies seriously. During the sign-up
   process, you can specify any allergies or dietary restrictions. Our
   team will ensure that your meals are prepared accordingly, providing
   you with safe and delicious options."
         />
         <AccordionComponent
           question="How flexible is SavorSwift? What am I committing to?"
-          answer="SavourSwift is a flexible weekly subscription with no minimum
+          answer="SavorSwift is a flexible weekly subscription with no minimum
             commitment and no cancellation fees. You can change your recipes,
             modify your delivery, or skip weeks without any obligation."
         />
