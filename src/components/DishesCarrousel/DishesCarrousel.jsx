88e9094fe436d150b8a1eb4a5c21db1b76173e50
guzegui <guzmanegui@gmail.com>
2024-06-30 13:30:46 +0200

    Edit dish carrousel to get recipes from context, get 3 random ones to display and link to details

--- src/components/DishesCarrousel/DishesCarrousel.jsx
+++ src/components/DishesCarrousel/DishesCarrousel.jsx
@@ -1,42 +1,59 @@
 import { useState, useEffect, useContext } from "react";
-import axios from "axios";
 import "./DishesCarrousel.css";
-
-const MONGO_URI = process.env.REACT_APP_SERVER_URL
-  ? `${process.env.REACT_APP_SERVER_URL}/dishes`
-  : "http://localhost:5005/dishes";
+import { CartContext } from "../../context/cart.context";
+import Loading from "../Loading/Loading";
+import { Link } from "react-router-dom";
 
 function DishesCarrousel() {
   // Define recipes
   const [showRecipes, setShowRecipes] = useState([]);
+  const { recipes } = useContext(CartContext);
+
+  // Get 3 random numbers between 0 and recipes.length
+  const get3RandomNumbers = Array.from({ length: 3 }, () =>
+    Math.floor(Math.random() * recipes.length)
+  );
 
   // Get recipes from the database
   useEffect(() => {
-    axios
-      .get(MONGO_URI)
-      .then((res) => {
-        setShowRecipes(res.data.slice(0, 3)); // Set showRecipes to the first 3 items
-      })
-      .catch((err) => console.error("Error fetching recipes:", err));
-  }, []);
+    // get 3 random recipes from recipes array
+    if (recipes?.length > 0) {
+      const showcasedRecipes = get3RandomNumbers.map((index) => recipes[index]);
+      setShowRecipes(showcasedRecipes); // Set showRecipes to the first 3 items
+    }
+  }, [recipes]);
+
+  if (!showRecipes) {
+    <Loading />;
+  }
 
   return (
-    <div>
-      <h3>Look at some of our dishes</h3>
-      <section className="carrousel-main">
-        {showRecipes &&
-          showRecipes.map((recipe, index) => (
-            <div 
-            className="recipe-showcase-container"
-            key={index}
-            style={{ backgroundImage: `url(/${encodeURIComponent(recipe.name)}.jpg)`}}
-            >
-              {/* <img src={`/${recipe.name}.jpg`} alt={`${recipe.name}`}></img> */}
-              <h4>{recipe.name}</h4>
-            </div>
-          ))}
-      </section>
-    </div>
+    <>
+      {showRecipes && recipes && (
+        <div>
+          <h3>Look at some of our dishes</h3>
+          <section className="carrousel-main">
+            {showRecipes &&
+              showRecipes.map((recipe, index) => (
+                <Link to={`/recipes/${recipe._id}`}>
+                  <div
+                    className="recipe-showcase-container"
+                    key={index}
+                    style={{
+                      backgroundImage: `url(/${encodeURIComponent(
+                        recipe.name
+                      )}.jpg)`,
+                    }}
+                  >
+                    {/* <img src={`/${recipe.name}.jpg`} alt={`${recipe.name}`}></img> */}
+                    <h4>{recipe.name}</h4>
+                  </div>
+                </Link>
+              ))}
+          </section>
+        </div>
+      )}
+    </>
   );
 }
 
