ed5b0f33ed03b4980389a42c32a1928e92cfe826
Abraham Agusti <abraham.agusti@gmail.com>
2024-06-30 12:11:41 +0200

    signUpCss

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -2,64 +2,80 @@ import "./SignupPage.css";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import authService from "../../services/auth.service";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Box from "@mui/material/Box";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirmation, setShowPasswordConfirmation] =
-    useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
-  const [passwordsMatch, setPasswordsMatch] = useState(true);
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordConfirmationError, setPasswordConfirmationError] = useState(false);
 
   const navigate = useNavigate();
 
-  const handleEmail = (e) => setEmail(e.target.value);
-  const handlePassword = (e) => setPassword(e.target.value);
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setEmailError(false);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setPasswordError(false);
+  };
+
   const handleName = (e) => setName(e.target.value);
   const handleLastName = (e) => setLastName(e.target.value);
-  const handlePasswordConfirmation = (e) =>
+
+  const handlePasswordConfirmation = (e) => {
     setPasswordConfirmation(e.target.value);
+    setPasswordConfirmationError(false);
+  };
 
   const handleSignupSubmit = async (e) => {
     e.preventDefault();
+    let valid = true;
+
+    if (!email) {
+      setEmailError(true);
+      valid = false;
+    }
+
+    if (!password) {
+      setPasswordError(true);
+      valid = false;
+    }
+
     if (password !== passwordConfirmation) {
-      setPasswordsMatch(false);
+      setPasswordConfirmationError(true);
+      valid = false;
+    }
+
+    if (!valid) {
       return;
     }
-    // Create an object representing the request body
+
     try {
       const requestBody = { email, password, name, lastName };
-
-      // add for auto logi un once signed up
-
       const response = await authService.signup(requestBody);
       const authToken = response.data.token;
       localStorage.setItem("authToken", authToken);
       navigate("/mealplan");
-
-      // Send a request to the server using axios
-      /* 
-    const authToken = localStorage.getItem("authToken");
-    axios.post(
-      `${process.env.REACT_APP_SERVER_URL}/auth/signup`, 
-      requestBody, 
-      { headers: { Authorization: `Bearer ${authToken}` },
-    })
-    .then((response) => {})
-    */
-
-      // Or using a service
     } catch (error) {
-      // Handling errors
-      if (
-        error.response &&
-        error.response.data &&
-        error.response.data.message
-      ) {
+      if (error.response && error.response.data && error.response.data.message) {
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
       } else {
@@ -68,7 +84,6 @@ function SignupPage() {
     }
   };
 
-  // Functions to handle password visibility
   const handleTogglePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
   };
@@ -77,69 +92,105 @@ function SignupPage() {
     setShowPasswordConfirmation((prev) => !prev);
   };
 
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="SignupPage">
       <h1>Sign Up</h1>
-
-      <form onSubmit={handleSignupSubmit}>
-        <label>Email:</label>
-        <input type="email" name="email" value={email} onChange={handleEmail} />
-        <label>Password:</label>
-        <div className="password-input-container">
-          <input
-            type={showPassword ? "text" : "password"}
-            name="password"
+      <form onSubmit={handleSignupSubmit} className="form-control">
+        <TextField
+          error={emailError}
+          label="Email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleEmail}
+          fullWidth
+          margin="normal"
+          sx={{ backgroundColor: 'white' }}
+          variant="outlined"
+          inputProps={{ sx: { border: '1px solid yellow' } }}
+        />
+        <FormControl fullWidth margin="normal" variant="outlined" sx={{ backgroundColor: 'white' }}>
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
             value={password}
             onChange={handlePassword}
-            className="form-input"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleTogglePasswordVisibility}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            sx={{ backgroundColor: 'white' }}
+            variant="outlined"
+            inputProps={{ sx: { border: '1px solid yellow' } }}
           />
-          <input
-            type="checkbox"
-            className="password-toggle-checkbox"
-            checked={showPassword}
-            onChange={handleTogglePasswordVisibility}
-          />
-          
-        </div>
-
-        <label>Confirm Password:</label>
-        <div className="password-input-container">
-          <input
-            type={showPasswordConfirmation ? "text" : "password"}
-            name="passwordConfirmation"
+        </FormControl>
+        <FormControl fullWidth margin="normal" variant="outlined" sx={{ backgroundColor: 'white' }}>
+          <InputLabel htmlFor="outlined-adornment-password-confirmation">Confirm Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password-confirmation"
+            type={showPasswordConfirmation ? 'text' : 'password'}
             value={passwordConfirmation}
             onChange={handlePasswordConfirmation}
-            className="form-input"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleTogglePasswordConfirmationVisibility}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPasswordConfirmation ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Confirm Password"
+            sx={{ backgroundColor: 'white' }}
+            variant="outlined"
+            inputProps={{ sx: { border: '1px solid yellow' } }}
           />
-          <input
-            type="checkbox"
-            className="password-toggle-checkbox"
-            checked={showPasswordConfirmation}
-            onChange={handleTogglePasswordConfirmationVisibility}
-          />
-        </div>
-
-        {!passwordsMatch && (
-          <p className="error-message">
-            Passwords do not match. Please try again.
-          </p>
-        )}
-        <label>Name:</label>
-        <input type="text" name="name" value={name} onChange={handleName} />
-        <label>Last Name:</label>
-        <input
+        </FormControl>
+        <TextField
+          label="Name"
           type="text"
-          name="Last name"
+          name="name"
+          value={name}
+          onChange={handleName}
+          fullWidth
+          margin="normal"
+          sx={{ backgroundColor: 'white' }}
+          variant="outlined"
+          inputProps={{ sx: { border: '1px solid yellow' } }}
+        />
+        <TextField
+          label="Last Name"
+          type="text"
+          name="lastName"
           value={lastName}
           onChange={handleLastName}
+          fullWidth
+          margin="normal"
+          sx={{ backgroundColor: 'white' }}
+          variant="outlined"
+          inputProps={{ sx: { border: '1px solid yellow' } }}
         />
-        <button type="submit">Sign Up</button>
+        <button type="submit" className="submit-button">Sign Up</button>
       </form>
-
       {errorMessage && <p className="error-message">{errorMessage}</p>}
-
-      <p>Already have account?</p>
-      <Link to={"/login"}> Login</Link>
+      <p>Already have an account? <Link to={"/login"}>Login</Link></p>
     </div>
   );
 }
