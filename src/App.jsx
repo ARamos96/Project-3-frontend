81a0366e83cc9d9a16868499c5b0fae7f2a31c24
guzegui <guzmanegui@gmail.com>
2025-04-22 15:02:27 +0200

    add fade-in effect for main content. TODO lazy loading, debloat

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./styles/main.scss";
 import { Routes, Route } from "react-router-dom";
-
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
-
 import Footer from "./components/Footer/Footer";
-
 import HomePage from "./pages/HomePage/HomePage";
 import NewProfilePage from "./pages/ProfilePage/NewProfilePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
@@ -14,65 +12,79 @@ import RecipesPage from "./pages/RecipesPage/RecipesPage";
 import RecipeDetailsPage from "./pages/RecipeDetailsPage/RecipeDetailsPage";
 import MealPlan from "./pages/MealPlan/MealPlan";
 import CheckOut from "./pages/Checkout/CheckOut";
-
 import IsPrivate from "./components/IsPrivate/IsPrivate";
 import IsAnon from "./components/IsAnon/IsAnon";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [showMainContent, setShowMainContent] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowMainContent(true);
+    }, 1000); // matches the typing animation duration
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="App">
       <div className="page-container">
         <div className="top-heading">
           <h1>
-            <span className="typewriter-text">SavorSwift!</span>
+            {!showMainContent ? (
+              <span className="typewriter-text">SavorSwift!</span>
+            ) : (
+              "SavorSwift!"
+            )}
           </h1>
         </div>
 
-        <Navbar />
-        <ToastContainer />
+        {showMainContent && (
+          <div className="fade-in">
+            <Navbar />
+            <ToastContainer />
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/profile"
-            element={
-              <IsPrivate>
-                <NewProfilePage />
-              </IsPrivate>
-            }
-          />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route
+                path="/profile"
+                element={
+                  <IsPrivate>
+                    <NewProfilePage />
+                  </IsPrivate>
+                }
+              />
+              <Route
+                path="/signup"
+                element={
+                  <IsAnon>
+                    <SignupPage />
+                  </IsAnon>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <IsAnon>
+                    <LoginPage />
+                  </IsAnon>
+                }
+              />
+              <Route path="/mealplan" element={<MealPlan />} />
+              <Route path="/checkout" element={<CheckOut />} />
+              <Route path="/howitworks" element={<HowItWorksPage />} />
+              <Route path="/recipes" element={<RecipesPage />} />
+              <Route
+                path="/recipes/:recipeId"
+                element={<RecipeDetailsPage />}
+              />
+            </Routes>
 
-          <Route
-            path="/signup"
-            element={
-              <IsAnon>
-                <SignupPage />
-              </IsAnon>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <IsAnon>
-                <LoginPage />
-              </IsAnon>
-            }
-          />
-
-          <Route path="/mealplan" element={<MealPlan />} />
-
-          <Route path="/checkout" element={<CheckOut />} />
-
-          <Route path="/howitworks" element={<HowItWorksPage />} />
-
-          <Route path="/recipes" element={<RecipesPage />} />
-          <Route path="/recipes/:recipeId" element={<RecipeDetailsPage />} />
-        </Routes>
-
-        <Footer />
+            <Footer />
+          </div>
+        )}
       </div>
     </div>
   );
